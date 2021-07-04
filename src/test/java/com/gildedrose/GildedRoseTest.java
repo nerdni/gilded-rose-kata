0f1e6c02f94f34bab655d27224f2d93919ecce2a
Indre <indrenem@gmail.com>
2021-07-04 13:32:04 +0300

    tests added

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,17 +1,112 @@
 package com.gildedrose;
 
-import org.junit.jupiter.api.Test;
+import com.gildedrose.model.Item;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
-    @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("itemName", 0, 0) };
-        GildedRose app = new GildedRose(items);
+    @ParameterizedTest
+    @MethodSource("withSulfuras")
+    void testWhenSulfuras(Item initialItem, int expectedSellIn, int expectedQuality) {
+
+        GildedRose app = new GildedRose(new Item[]{initialItem});
+
         app.updateQuality();
-        assertEquals("itemName", app.getItems()[0].name);
+
+        Item item = app.getItems()[0];
+        assertEquals(initialItem.name, item.name);
+        assertEquals(expectedSellIn, item.sellIn);
+        assertEquals(expectedQuality, item.quality);
     }
 
+    @ParameterizedTest
+    @MethodSource("withRegularItems")
+    void testWhenRegularItems(Item initialItem, int expectedSellIn, int expectedQuality) {
+
+        GildedRose app = new GildedRose(new Item[]{initialItem});
+
+        app.updateQuality();
+
+        Item item = app.getItems()[0];
+        assertEquals(initialItem.name, item.name);
+        assertEquals(expectedSellIn, item.sellIn);
+        assertEquals(expectedQuality, item.quality);
+    }
+
+    @ParameterizedTest
+    @MethodSource("withAgedBrie")
+    void testWhenAgedBrie(Item initialItem, int expectedSellIn, int expectedQuality) {
+
+        GildedRose app = new GildedRose(new Item[]{initialItem});
+
+        app.updateQuality();
+
+        Item item = app.getItems()[0];
+        assertEquals(initialItem.name, item.name);
+        assertEquals(expectedSellIn, item.sellIn);
+        assertEquals(expectedQuality, item.quality);
+    }
+
+    @ParameterizedTest
+    @MethodSource("withBackStageTickets")
+    void testWhenBackStageTickets(Item initialItem, int expectedSellIn, int expectedQuality) {
+
+        GildedRose app = new GildedRose(new Item[]{initialItem});
+
+        app.updateQuality();
+
+        Item item = app.getItems()[0];
+        assertEquals(initialItem.name, item.name);
+        assertEquals(expectedSellIn, item.sellIn);
+        assertEquals(expectedQuality, item.quality);
+    }
+
+    private static Stream<Arguments> withAgedBrie() {
+        String agedBrie = "Aged Brie";
+        return Stream.of(
+                Arguments.of(new Item(agedBrie, 10, 0), 9, 1),
+                Arguments.of(new Item(agedBrie, 10, 50), 9, 50),
+                Arguments.of(new Item(agedBrie, 0, 10), -1, 12),
+                Arguments.of(new Item(agedBrie, 0, 0), -1, 2)
+        );
+    }
+
+    private static Stream<Arguments> withRegularItems() {
+        String regularItem = "regular item";
+        return Stream.of(
+                Arguments.of(new Item(regularItem, 10, 0), 9, 0),
+                Arguments.of(new Item(regularItem, 10, 10), 9, 9),
+                Arguments.of(new Item(regularItem, 0, 10), -1, 8),
+                Arguments.of(new Item(regularItem, 0, 0), -1, 0)
+        );
+    }
+
+    private static Stream<Arguments> withSulfuras() {
+        String sulfuras = "Sulfuras, Hand of Ragnaros";
+        return Stream.of(
+                Arguments.of(new Item(sulfuras, 0, 80), 0, 80),
+                Arguments.of(new Item(sulfuras, 0, 80), 0, 80),
+                Arguments.of(new Item(sulfuras, 10, 80), 10, 80)
+        );
+    }
+
+    private static Stream<Arguments> withBackStageTickets() {
+        String backstageTickets = "Backstage passes to a TAFKAL80ETC concert";
+
+        return Stream.of(
+                Arguments.of(new Item(backstageTickets, 0, 10), -1, 0),
+                Arguments.of(new Item(backstageTickets, 20, 50), 19, 50),
+                //10 before concert quality+2
+                Arguments.of(new Item(backstageTickets, 10, 0), 9, 2),
+                Arguments.of(new Item(backstageTickets, 10, 49), 9, 50),
+                //5 before concert quality+3
+                Arguments.of(new Item(backstageTickets, 5, 0), 4, 3)
+        );
+    }
 }
